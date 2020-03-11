a0868f0bcdd75857c397e5bbc3bb10079443b0b4
wuyujun <wuyj@tranzvision.com.cn>
2020-03-11 23:37:06 +0800

    轮播图实现
    轮播图实现

--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -3,26 +3,31 @@
 		<nav-bar  class='home-nav'>
 			<div slot='center'>购物车</div>
 		</nav-bar>
-		<div>{{banner}}</div>
+		<ban-ner :sliders='banner'></ban-ner>
 	</div>
 </template>
 
 <script>
 	import NavBar from 'components/common/navbar/NavBar'
+	import BanNer from 'components/common/banner/BanNer'
 	import {getHomeMultidata} from 'network/home'
 	export default {
 		name:'Home',
 		components:{
-			NavBar
+			NavBar,
+			BanNer
 		},
 		created(){
 			//请求多个数据
 			getHomeMultidata().then(
 				res =>{
-					this.banner = res.data.banner.list;
+					this.banner = res.data.banner.list.map(valueMap =>{
+						return {img:valueMap.image};
+					});
 					this.dKeyword = res.data.dKeyword.list;
 					this.keywords = res.data.keywords.list;
 					this.recommend = res.data.recommend.list;
+					console.log(this.banner)
 				},err =>{
 
 				})
